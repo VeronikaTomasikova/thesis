b331b640ae82b960922d41c1b3bdf3cd9bf2252c
VeronikaTomasikova <payulovesrain@gmail.com>
2026-01-02 17:11:59 +0100

    Add files via upload

---
+++ EXS_code_ArduinoIDE/libraries/i2c_ext/I2C_EXT.cpp
@@ -0,0 +1,554 @@
+/**
+ * @file I2C_EXT.cpp
+ * @brief Library code for AD5934
+ */
+
+#include "I2C_EXT.h"
+#include <Adafruit_Sensor.h>               // For the unified sensor interface
+#include <Adafruit_BME680.h> 
+#include "TCA9548.h"
+#include <Math.h>
+
+/**
+ * Request to read a byte from the AD5934.
+ *
+ * @param address Address of register requesting data from
+ * @param value Pointer to a byte where the return value should be stored, or
+ *        where the error code will be stored if fail.
+ * @return Success or failure
+ */
+int AD5934::getByte(byte address, byte *value) {
+    Wire.beginTransmission(AD5934_ADDR);
+    Wire.write(ADDR_PTR);
+    Wire.write(address);
+
+    if (byte res = Wire.endTransmission() != I2C_RESULT_SUCCESS) {
+        *value = res;
+        return false;
+    }
+
+    Wire.requestFrom(AD5934_ADDR, 1);
+    if (Wire.available()) {
+        *value = Wire.read();
+        return true;
+    } else {
+        *value = 0;
+        return false;
+    }
+}
+
+/**
+ * Write a byte to a register on the AD5934.
+ *
+ * @param address The register address to write to
+ * @param value The byte to write to the address
+ * @return Success or failure of transmission
+ */
+bool AD5934::sendByte(byte address, byte value) {
+    Wire.beginTransmission(AD5934_ADDR);
+    Wire.write(address);
+    Wire.write(value);
+
+    if (Wire.endTransmission() != I2C_RESULT_SUCCESS) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool AD5934::setControlMode(byte mode) {
+    byte val;
+    if (!getByte(CTRL_REG1, &val))
+        return false;
+
+    // Clear the top 4 bits (mode bits) and set new mode
+    val &= 0x0F;
+
+    // Set the top 4 bits appropriately
+    val |= mode;
+
+    return sendByte(CTRL_REG1, val);
+}
+
+/**
+ * Reset the AD5934.
+ *
+ * @return Success or failure
+ */
+bool AD5934::reset() {
+    byte val;
+    if (!getByte(CTRL_REG2, &val))
+        return false;
+
+    val |= CTRL_RESET;
+    return sendByte(CTRL_REG2, val);
+}
+
+/**
+ * Set the external clock as required for AD5934.
+ *
+ * @return Success or failure
+ */
+bool AD5934::setExternalClock() {
+    return sendByte(CTRL_REG2, CLOCK_EXTERNAL); 
+}
+
+/**
+ * Set the settling time cycles use for frequency sweep.
+ *
+ * @param time The settling time cycles to set.
+ * @return Success or failure
+ */
+bool AD5934::setSettlingCycles(int time) { //for clock and synchronisation
+    int cycles;
+    byte settleTime[2], rsTime[2], val;
+
+    settleTime[0] = time & 0xFF;
+    settleTime[1] = (time >> 8) & 0xFF;
+
+    cycles = (settleTime[0] | (settleTime[1] & 0x1));
+    val = (byte)((settleTime[1] & 0x7) >> 1);
+
+    if ((cycles > 0x1FF) || !(val == 0 || val == 1 || val == 3)) {
+        return false;
+    }
+
+    if (sendByte(NUM_SCYCLES_1, settleTime[1]) && sendByte(NUM_SCYCLES_2, settleTime[0])) {
+        if (getByte(NUM_SCYCLES_1, &rsTime[1]) && getByte(NUM_SCYCLES_2, &rsTime[0])) {
+            if ((settleTime[0] == rsTime[0]) && (settleTime[1] == rsTime[1])) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+/**
+ * Set the start frequency for a frequency sweep.
+ *
+ * @param start The initial frequency.
+ * @return Success or failure
+ */
+bool AD5934::setStartFrequency(unsigned long start) {
+    long freqHex = (start / (16e6 / 16)) * pow(2, 27);
+    if (freqHex > 0xFFFFFF) {
+        return false;
+    }
+
+    byte highByte = (freqHex >> 16) & 0xFF;
+    byte midByte = (freqHex >> 8) & 0xFF;
+    byte lowByte = freqHex & 0xFF;
+
+    return sendByte(START_FREQ_1, highByte) &&
+           sendByte(START_FREQ_2, midByte) &&
+           sendByte(START_FREQ_3, lowByte);
+}
+
+/**
+ * Set the increment frequency for a frequency sweep.
+ *
+ * @param increment The frequency to increment by, 0x430.
+ * @return Success or failure
+ */
+bool AD5934::setIncrementFrequency(unsigned long increment) { //for setting how much frequencies are changed and incremented during frequency sweep
+    long freqHex = (increment / (16e6 / 16)) * pow(2, 27);
+    if (freqHex > 0xFFFFFF) {
+        return false;
+    }
+
+    byte highByte = (freqHex >> 16) & 0xFF;
+    byte midByte = (freqHex >> 8) & 0xFF;
+    byte lowByte = freqHex & 0xFF;
+
+    return sendByte(INC_FREQ_1, highByte) &&
+           sendByte(INC_FREQ_2, midByte) &&
+           sendByte(INC_FREQ_3, lowByte);
+}
+
+/**
+ * Set the number of frequency increments for a frequency sweep.
+ *
+ * @param num The number of increments.
+ * @return Success or failure
+ */
+bool AD5934::setNumberIncrements(unsigned int num) { //til when the sweep goes, how much increment steps it takes
+    if (num > 511) {
+        return false;
+    }
+
+    byte highByte = (num >> 8) & 0xFF;
+    byte lowByte = num & 0xFF;
+
+    return sendByte(NUM_INC_1, highByte) &&
+           sendByte(NUM_INC_2, lowByte);
+}
+
+/**
+ * Set the PGA gain factor.
+ *
+ * @param gain The gain factor to select. Use constants or 1/5.
+ * @return Success or failure
+ */
+bool AD5934::setPGAGain(byte gain) { //choses feedback resistor based on setting this bit- either 1 or 0 => 1 or 5, for the amplifier inside the chip
+    byte val;
+    if (!getByte(CTRL_REG1, &val))
+        return false;
+
+    val &= 0xFE;
+
+    if (gain == PGA_GAIN_X1 || gain == 1) {
+        val |= PGA_GAIN_X1;
+        return sendByte(CTRL_REG1, val);
+    } else if (gain == PGA_GAIN_X5 || gain == 5) {
+        val |= PGA_GAIN_X5;
+        return sendByte(CTRL_REG1, val);
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Read the value of a register.
+ *
+ * @param reg The address of the register to read.
+ * @return The value of the register. Returns 0xFF if can't read it.
+ */
+byte AD5934::readRegister(byte reg) {
+    byte val;
+    if (getByte(reg, &val)) {
+        return val;
+    } else {
+        return STATUS_ERROR;
+    }
+}
+
+bool AD5934::setRange(byte range) { //for setting the output voltage
+    byte val;
+
+    if (!getByte(CTRL_REG1, &val))
+        return false;
+
+    // Clear bits D9 and D10 (voltage range bits)
+    val &= 0xF9;
+
+    // Set range bits based on the selected range
+    switch (range) {
+        case CTRL_OUTPUT_RANGE_2:
+            val |= CTRL_OUTPUT_RANGE_2;
+            break;
+        case CTRL_OUTPUT_RANGE_3:
+            val |= CTRL_OUTPUT_RANGE_3;
+            break;
+        case CTRL_OUTPUT_RANGE_4:
+            val |= CTRL_OUTPUT_RANGE_4;
+            break;
+        default:
+            val |= CTRL_OUTPUT_RANGE_1;
+            break;
+    }
+
+    return sendByte(CTRL_REG1, val);
+}
+
+/**
+ * Read the value of the status register.
+ *
+ * @return The value of the status register. Returns 0xFF if can't read it.
+ */
+byte AD5934::readStatusRegister() {
+    return readRegister(STATUS_REG);
+}
+
+/**
+ * Read the value of the control register.
+ *
+ * @return The value of the control register. Returns 0xFFFF if can't read it.
+ */
+int AD5934::readControlRegister() {
+    return ((readRegister(CTRL_REG1) << 8) | readRegister(CTRL_REG2)) & 0xFFFF;
+}
+
+/**
+ * Get a raw complex number for a specific frequency measurement.
+ *
+ * @param real Pointer to an int that will contain the real component.
+ * @param imag Pointer to an int that will contain the imaginary component.
+ * @return Success or failure
+ */
+bool AD5934::getComplexData(int *real, int *imag) {
+    // Wait for a measurement to be available
+    while ((readStatusRegister() & STATUS_DATA_VALID) != STATUS_DATA_VALID);
+
+    byte realComp[2];
+    byte imagComp[2];
+    if (getByte(REAL_DATA_1, &realComp[0]) &&
+        getByte(REAL_DATA_2, &realComp[1]) &&
+        getByte(IMAG_DATA_1, &imagComp[0]) &&
+        getByte(IMAG_DATA_2, &imagComp[1])) {
+        // Combine the two separate bytes into a single 16-bit value and store
+        // them at the locations specified.
+        *real = (int16_t)(((realComp[0] << 8) | realComp[1]) & 0xFFFF);
+        *imag = (int16_t)(((imagComp[0] << 8) | imagComp[1]) & 0xFFFF);
+        return true;
+    } else {
+        *real = -1;
+        *imag = -1;
+        return false;
+    }
+}
+
+/**
+ * Set the power level of the AD5934.
+ *
+ * @param level The power level to choose.
+ * @return Success or failure
+ */
+bool AD5934::setPowerMode(byte level) {
+    switch (level) {
+        case POWER_ON:
+            return setControlMode(CTRL_NO_OPERATION);
+        case POWER_STANDBY:
+            return setControlMode(CTRL_STANDBY_MODE);
+        case POWER_DOWN:
+            return setControlMode(CTRL_POWER_DOWN_MODE);
+        default:
+            return false;
+    }
+}
+
+//-----------------------------------------------FREQUENCY SWEEP---------------------------------------------------------
+bool AD5934::frequencySweep(int real[], int imag[], int n) {
+    // Initialize sweep
+    //Serial.println("Initializing frequency sweep...");
+    if (!(setPowerMode(POWER_STANDBY) &&
+          setControlMode(CTRL_INIT_START_FREQ) &&
+          setControlMode(CTRL_START_FREQ_SWEEP))) {
+        return false;
+    }
+
+    // Perform sweep
+    int i = 0;
+    while ((readStatusRegister() & STATUS_SWEEP_DONE) != STATUS_SWEEP_DONE) {
+        //delay(15); //testing proved this obsolete
+        if (i >= n) return false;
+
+        // Wait for valid data
+        while ((readStatusRegister() & STATUS_DATA_VALID) != STATUS_DATA_VALID) {
+            delay(20); // Prevent overloading the I2C bus. with testing reduced to 20. below that doesnt really save time
+        }
+
+        // Retrieve impedance data
+        if (!getComplexData(&real[i], &imag[i])) {
+            return false; // Failed to retrieve data
+        }
+
+        // Increment frequency
+        if (!setControlMode(CTRL_INCREMENT_FREQ)) {
+            return false; // Failed to increment frequency
+        }
+
+        delay(20); // Delay after increment. with testing reduced to 20. below that doesnt really save time
+        i++;
+    }
+    delay(50); // Allow the device to stabilize
+    return setPowerMode(POWER_STANDBY); // Put device into standby
+}
+
+
+/**
+ * Computes the gain factor and phase for each point in a frequency sweep.
+ *
+ * @param gain An array of appropriate size to hold the gain factors
+ * @param phase An array of appropriate size to hold phase data.
+ * @param ref The known reference resistance.
+ * @param n Length of the array (or the number of discrete measurements)
+ * @return Success or failure
+ */
+bool AD5934::calibrate(double gain[], int phase[], int ref, int n) {
+    int *real = new int[n];
+    int *imag = new int[n];
+
+    if (!frequencySweep(real, imag, n)) {
+        delete[] real;
+        delete[] imag;
+        return false;
+    }
+
+    for (int i = 0; i < n; i++) {
+        gain[i] = (double)(1.0 / ref) / sqrt(pow(real[i], 2) + pow(imag[i], 2));
+        phase[i] = atan2(imag[i], real[i]) * (180.0 / M_PI);  // Convert to degrees if needed /find value of i
+
+    }
+
+    delete[] real;
+    delete[] imag;
+    return true;
+}
+
+/**
+ * Computes the gain factor and phase for each point in a frequency sweep.
+ * Also provides the caller with the real and imaginary data.
+ *
+ * @param gain An array of appropriate size to hold the gain factors
+ * @param phase An array of appropriate size to hold the phase data
+ * @param real An array of appropriate size to hold the real data
+ * @param imag An array of appropriate size to hold the imaginary data.
+ * @param ref The known reference resistance.
+ * @param n Length of the array (or the number of discrete measurements)
+ * @return Success or failure
+ */
+bool AD5934::calibrate(double gain[], int phase[], int real[], int imag[], int ref, int n) {
+    if (!frequencySweep(real, imag, n)) {
+        return false;
+    }
+
+    for (int i = 0; i < n; i++) {
+        gain[i] = (double)(1.0 / ref) / sqrt(pow(real[i], 2) + pow(imag[i], 2));
+        phase[i] = atan2(imag[i], real[i]) * (180.0 / M_PI);  // Always convert phase to degrees
+    }
+
+    return true;
+}
+
+/*---------------------------------------------------------SWEEP WITH MULTIPLEXER----------------------------------------------------------*/
+//DEBUGGING VERSION
+void SweepAndProcess(uint8_t channel, AD5934 &device, TCA9548 &multiplexer, 
+                     float magnitudeData[][NUM_INCREMENTS], float phaseData[][NUM_INCREMENTS], int deviceIndex) {
+   
+
+    // Debug message
+    //Serial.print("Starting frequency sweep on channel ");
+    //Serial.println(channel);
+
+    // Temporary arrays for raw sweep data
+    int realData[NUM_INCREMENTS];
+    int imagData[NUM_INCREMENTS];
+
+    // Perform the sweep
+    if (!device.frequencySweep(realData, imagData, NUM_INCREMENTS)) {
+        //Serial.print("Frequency sweep failed on channel ");
+        //Serial.println(channel);
+        return;
+    }
+
+    //Serial.print("Frequency sweep completed on channel ");
+    //Serial.println(channel);
+    //Serial.print("Number of data points: ");
+    //Serial.println(NUM_INCREMENTS);
+
+    // Save and print data for this device
+    for (int i = 0; i < NUM_INCREMENTS; i++) {
+        magnitudeData[deviceIndex][i] = sqrt(pow(realData[i], 2) + pow(imagData[i], 2));
+        phaseData[deviceIndex][i] = atan2(imagData[i], realData[i]) * (180.0 / PI);
+
+        // Print each data point
+        //Serial.print("Point ");
+        //Serial.print(i + 1);
+        //Serial.print(": Real = ");
+        //Serial.print(realData[i]);
+        //Serial.print(", Imag = ");
+        //Serial.print(imagData[i]);
+        //Serial.print(", Magnitude = ");
+        //Serial.print(magnitudeData[deviceIndex][i]);
+        //Serial.print(", Phase = ");
+        //Serial.print(phaseData[deviceIndex][i]);
+        //Serial.println(" degrees");
+
+        //delay(100); // Add delay here (200ms = 0.2 seconds per point, adjust as needed)
+    }
+
+    // Debug message for power down
+    //Serial.print("Powering down AD5934 on channel ");
+    //Serial.println(channel);
+    device.setPowerMode(POWER_DOWN);
+}
+
+
+
+
+
+//NO DEBUGGING
+/*void SweepAndProcess(uint8_t channel, AD5934 &device, TCA9548 &multiplexer, 
+                     float magnitudeData[][NUM_INCREMENTS], float phaseData[][NUM_INCREMENTS], int deviceIndex) {
+    multiplexer.selectChannel(channel);
+
+    // Temporary arrays for raw sweep data
+    int realData[NUM_INCREMENTS];
+    int imagData[NUM_INCREMENTS];
+
+    // Perform the sweep
+    if (!device.frequencySweep(realData, imagData, NUM_INCREMENTS)) {
+        Serial.println("Frequency sweep failed");
+        return;
+    }
+
+    // Save the data for this device
+    for (int i = 0; i < NUM_INCREMENTS; i++) {
+        magnitudeData[deviceIndex][i] = sqrt(pow(realData[i], 2) + pow(imagData[i], 2));
+        phaseData[deviceIndex][i] = atan2(imagData[i], realData[i]) * (180.0 / PI);
+
+    }
+    device.setPowerMode(POWER_DOWN);
+}*/
+
+
+/*-----------------------------------------------------AD5934 INITIALIZATION SEQUENCE------------------------------------------------------*/
+bool AD5934::setupAD5934( unsigned long startFrequency) {
+    if (!(setExternalClock() &&
+          setStartFrequency(startFrequency) &&  // Use the passed startFrequency
+          setNumberIncrements(NUM_INCREMENTS) &&
+          setIncrementFrequency(STEP_SIZE) &&
+          setRange(CTRL_OUTPUT_RANGE_1))) {
+        return false;
+    }
+
+    // Configure settling cycles
+    if (!setSettlingCycles(SETTLING_CYCLES)) {  // Using the instance here
+        return false;
+    } 
+
+    // Set device to Standby Mode and initialize start frequency
+    if (!(setControlMode(CTRL_STANDBY_MODE) &&
+          setControlMode(CTRL_INIT_START_FREQ))) {
+        return false;
+    }
+
+    return true;
+}
+
+/*-----------------------------------------------------------ADAFRUIT BME680--------------------------------------------------------------*/
+
+//Initialization sequence for BME680
+bool setupBME680(Adafruit_BME680 &bme) {
+    if (!bme.begin(0x76)) {
+        //Serial.println("Failed to initialize BME680 sensor!");
+        return false;
+    }
+    //Serial.println("BME680 sensor detected and initialized.");
+    // Set up oversampling and filter settings
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+
+    //Serial.println("BME680 setup complete.");
+    return true;
+}
+
+
+/*-------------------------------------------------------------Data Send------------------------------------------------------------------*/
+void sendDataToRaspberryPi(float phaseData[], float temperature, float humidity) {
+    for(int i=0;i<NUM_INCREMENTS;i++){
+        Serial.print(phaseData[i], 3);
+        if(i!=NUM_INCREMENTS-1){
+             Serial.write(44);
+        }
+    }
+    Serial.write(38);
+    Serial.print(temperature);
+    Serial.write(38);
+    Serial.print(humidity);
+
+}
+
+
+
---
+++ EXS_code_ArduinoIDE/libraries/i2c_ext/I2C_EXT.h
@@ -0,0 +1,165 @@
+#ifndef I2C_EXT_h
+#define I2C_EXT_h
+
+/**
+ * Includes
+ */
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+#include "TCA9548.h"
+
+/**
+ * AD5934 Library class
+ *  Contains mainly functions for interfacing with the AD5934.
+ */
+class AD5934 {
+    public:
+        //setup AD5934 sequence
+        bool setupAD5934( unsigned long startFrequency);
+
+
+        // Reset the board
+        static bool reset(void);
+
+        // Clock configuration
+        static bool setExternalClock(void);  // Added this to explicitly set external clockclock option.
+        bool setSettlingCycles(int);
+
+        // Frequency sweep configuration
+        static bool setStartFrequency(unsigned long);
+        static bool setIncrementFrequency(unsigned long);
+        static bool setNumberIncrements(unsigned int);
+
+        // Gain configuration
+        static bool setPGAGain(byte);
+
+        // Excitation range configuration
+        bool setRange(byte);
+
+        // Read registers
+        static byte readRegister(byte);
+        static byte readStatusRegister(void);
+        static int readControlRegister(void);
+
+        // Impedance data
+        static bool getComplexData(int*, int*);
+
+        // Set control mode register (CTRL_REG1)
+        static bool setControlMode(byte);
+
+        // Power mode
+        static bool setPowerMode(byte);
+
+        // Perform frequency sweeps
+        static bool frequencySweep(int real[], int imag[], int);
+        static bool calibrate(double gain[], int phase[], int ref, int n);
+        static bool calibrate(double gain[], int phase[], int real[],
+                              int imag[], int ref, int n);
+    private:
+
+        // Sending/Receiving byte method, for easy re-use
+        static int getByte(byte, byte*);
+        static bool sendByte(byte, byte);
+};
+
+
+/**
+ * AD5934 Register Map
+ *  Datasheet p20
+ */
+// Device address and address pointer p20
+#define AD5934_ADDR     (0x0D)
+#define ADDR_PTR        (0xB0)
+// Control Register
+#define CTRL_REG1       (0x80)
+#define CTRL_REG2       (0x81)
+// Start Frequency Register
+#define START_FREQ_1    (0x82)
+#define START_FREQ_2    (0x83)
+#define START_FREQ_3    (0x84)
+// Frequency increment register
+#define INC_FREQ_1      (0x85)
+#define INC_FREQ_2      (0x86)
+#define INC_FREQ_3      (0x87)
+// Number of increments register
+#define NUM_INC_1       (0x88)
+#define NUM_INC_2       (0x89)
+// Number of settling time cycles register
+#define NUM_SCYCLES_1   (0x8A)
+#define NUM_SCYCLES_2   (0x8B)
+// Status register
+#define STATUS_REG      (0x8F)
+// Real data register
+#define REAL_DATA_1     (0x94)
+#define REAL_DATA_2     (0x95)
+// Imaginary data register
+#define IMAG_DATA_1     (0x96)
+#define IMAG_DATA_2     (0x97)
+
+/**
+ * Constants
+ *  Constants for use with the AD5934 library class.
+ */
+// Clock sources
+#define CLOCK_EXTERNAL  (CTRL_CLOCK_EXTERNAL)
+// PGA gain options
+#define PGA_GAIN_X1     (CTRL_PGA_GAIN_X1)
+#define PGA_GAIN_X5     (CTRL_PGA_GAIN_X5)
+// Power modes
+#define POWER_STANDBY   (CTRL_STANDBY_MODE)
+#define POWER_DOWN      (CTRL_POWER_DOWN_MODE)
+#define POWER_ON        (CTRL_NO_OPERATION)
+// I2C result success/fail
+#define I2C_RESULT_SUCCESS       (0)
+#define I2C_RESULT_DATA_TOO_LONG (1)
+#define I2C_RESULT_ADDR_NAK      (2)
+#define I2C_RESULT_DATA_NAK      (3)
+#define I2C_RESULT_OTHER_FAIL    (4)
+// Control output voltage range options p20
+#define CTRL_OUTPUT_RANGE_1     (0b00000000) //2.0 V p-p typical
+#define CTRL_OUTPUT_RANGE_2     (0b00000110) //1.0 V p-p typical
+#define CTRL_OUTPUT_RANGE_3     (0b00000010) //200 mV p-p typical
+#define CTRL_OUTPUT_RANGE_4     (0b00000100) //400 mV p-p typical
+// Control register options
+#define CTRL_NO_OPERATION       (0b00000000)
+#define CTRL_INIT_START_FREQ    (0b00010000)
+#define CTRL_START_FREQ_SWEEP   (0b00100000)
+#define CTRL_INCREMENT_FREQ     (0b00110000)
+#define CTRL_REPEAT_FREQ        (0b01000000)
+#define CTRL_POWER_DOWN_MODE    (0b10100000)
+#define CTRL_STANDBY_MODE       (0b10110000)
+#define CTRL_RESET              (0b00010000)
+#define CTRL_CLOCK_EXTERNAL     (0b00001000)
+#define CTRL_PGA_GAIN_X1        (0b00000001)
+#define CTRL_PGA_GAIN_X5        (0b00000000)
+// Status register options
+#define STATUS_DATA_VALID       (0x02) //Valid real/imaginary data p22
+#define STATUS_SWEEP_DONE       (0x04) //Frequency sweep complete p22
+#define STATUS_ERROR            (0xFF)
+// Frequency sweep parameters
+#define SWEEP_DELAY             (1)
+
+//#define START_FREQUENCY1 14484 //microcantilever specific resonant frequency minus half of the number of increments
+//#define START_FREQUENCY2 16830 //microcantilever specific resonant frequency minus half of the number of increments
+//#define START_FREQUENCY3 15560 //microcantilever specific resonant frequency minus half of the number of increments
+
+#define NUM_INCREMENTS  501
+#define STEP_SIZE 1 //increment frequency (1)
+#define SETTLING_CYCLES 20 // Define the settling cycles (adjust as needed)
+
+void SweepAndProcess(uint8_t channel, AD5934 &device, TCA9548 &multiplexer, 
+                     float magnitudeData[][NUM_INCREMENTS], float phaseData[][NUM_INCREMENTS], int deviceIndex);
+
+
+// BME680 initialization
+bool setupBME680(Adafruit_BME680 &bme);
+
+void sendDataToRaspberryPi(float phaseData[3][NUM_INCREMENTS], float temperature, float humidity);
+
+
+
+
+#endif
+
